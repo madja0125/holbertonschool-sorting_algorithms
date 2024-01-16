171c11c13b8ffedaa6379c237f9b47d9e6dfc9aa
madja0125 <dejesusmiguel788@gmail.com>
2024-01-16 23:34:06 +0000

    fixing header file missing prototypes

--- sort.h
+++ sort.h
@@ -24,3 +24,10 @@ typedef struct listint_s
 
 /* prototypes */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+
+
+
+#endif
