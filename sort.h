c0373e874389a3e6db824b1a676dccfdb312d6ef
madja0125 <dejesusmiguel788@gmail.com>
2024-01-18 01:16:53 +0000

    adding adv task 102

--- sort.h
+++ sort.h
@@ -29,6 +29,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
+void counting_sort(int *array, size_t size);
 
 
 #endif
